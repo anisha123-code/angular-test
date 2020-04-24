c765fb44a9fcd6a761bfe02fadbc971144bbc149
Anisha Chhetri <anisha.chhetri.cse20@heritageit.edu.in>
2020-04-24 19:42:36 +0530

    Add files via upload

---
+++ assignment/app.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+           .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController ($scope) {
+        $scope.input = "";
+        $scope.message = "";
+
+        $scope.display = function() {
+
+            var array = $scope.input.split(',');
+            var strlen = $scope.input.length;
+            var noOfItems = array.length
+
+            if( strlen > 0 )
+            {
+              if( noOfItems < 4 )
+              {
+                $scope.message = "Enjoy a lot!"
+              }
+              else
+              {
+                  $scope.message = "Too much!"
+              }
+            }
+            else {
+              $scope.message = "Please enter data first";
+            }
+
+        };
+    }
+})();
